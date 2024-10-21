a3bb580e1128733e4ad030db5816cbfaedb0bd7d
dktmax <dktmax02@gmail.com>
2024-10-21 02:16:13 -0300

    atividade de cadastro pokemom

---
+++ cadastropokemom.js
@@ -0,0 +1,37 @@
+// Com notificação ( " const ")
+
+// Obtendo as seguintes informações para cadrastar o pokemom :) nome do Pokemom , sexo , Level 
+
+// Nome dos Pokemom = "Poochyena" , "Zigzagoon" , "Dragonite" , "Dragonite" , "Dragonite" , "Poochyena" , "Wurmple" 
+ 
+// Informações dos pokemom 
+
+let Pokemomcadastrados = [
+    ["Poochyena", "M", 2] , 
+    ["Zigzagoon", "F", 2] , 
+    ["Dragonite" , "M" , 5] , 
+    ["Dragonite" , "F" , 5 ], 
+    ["Dragonite" , "F" , 5] , 
+    ["Poochyena" , "F" , 3 ], 
+    ["Wurmple" , "M" , 2] 
+    ];
+    
+    //Mensagem de saida Com " const "
+    
+    const notificaçao = " Cadastrado com sucesso " 
+    
+    // Mensagem de saida
+    
+    console.log( Pokemomcadastrados[0][0] + notificaçao)
+    
+    console.log( Pokemomcadastrados[1][0] + notificaçao)
+    
+    console.log( Pokemomcadastrados[2][0] + notificaçao)
+    
+    console.log( Pokemomcadastrados[3][0] + notificaçao)
+    
+    console.log( Pokemomcadastrados[4][0] + notificaçao)
+    
+    console.log( Pokemomcadastrados[5][0] + notificaçao)
+    
+    console.log( Pokemomcadastrados[6][0] + notificaçao)
